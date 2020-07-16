623805f21f81598b8f2c4e2f360458083e29de6d
Milan Shrestha <milanshrestha17@hotmail.com>
2020-07-15 22:46:00 -0400

    Added scripts to sort astronauts, change color based on active=true and total num of astronauts

--- script.js
+++ script.js
@@ -2,20 +2,29 @@ window.addEventListener("load", function() {
 
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function(json) {
+
+            let newJson = json.sort(function(a, b) { return b.hoursInSpace - a.hoursInSpace});
             const container = document.getElementById("container");
 
-            for (let i = 0; i < json.length; i++) {
+            let count = newJson.length;
+            container.innerHTML = `
+                <div class="astronautCount">
+                    <h4>Total number of Astronauts: ${count}</h4>
+                </div>
+            `;
+
+            for (let i = 0; i < newJson.length; i++) {
                 container.innerHTML += `
                     <div class="astronaut">
                         <div class="bio">
-                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <h3>${newJson[i].firstName} ${newJson[i].lastName}</h3>
                             <ul>
-                                <li>Hours in space: ${json[i].hoursInSpace}</li>
-                                <li>Active: ${json[i].active}</li>
-                                <li>Skills: ${json[i].skills.join(", ")}</li>
+                                <li>Hours in space: ${newJson[i].hoursInSpace}</li>
+                                ${printActive(newJson[i])}
+                                <li>Skills: ${newJson[i].skills.join(", ")}</li>
                             </ul>
                         </div>
-                    <img class="avatar" src="${json[i].picture}">
+                    <img class="avatar" src="${newJson[i].picture}">
                     </div>
                 `
             };
@@ -23,3 +32,11 @@ window.addEventListener("load", function() {
         });
     });
 });
+
+function printActive(json) {
+
+    if (json.active === true) {
+        return `<li class="textColor">Active: true</li>`;
+    }
+    return `<li>Active: false</li>`;
+}
